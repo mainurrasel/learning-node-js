bf031dbf562ca7af0a855828033824053d42be11
Mainur Rasel <mainur.rahman@cellosco.pe>
2021-08-31 10:21:26 +0600

    folder added

---
+++ hapi-rest-api/index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, response) => {
+      return "Welcome to the rest API";
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/developer/{username}",
+    handler: (request, response) => {
+      var developerMock = {};
+      if (request.params.username == "rasel") {
+        developerMock = {
+          username: "rasel",
+          password: "amarami",
+          email: "mainurrrasel@gmail.com",
+        };
+      }
+      return developerMock;
+    },
+  });
+
+  server.route({
+    method: "POST",
+    path: "/developer",
+    handler: (request, response) => {
+      return (request.payload);
+    }
+  });
+
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
